fa3c38710112eca38424e2b59162b056700422f8
Kyle Hutchens <k.d.hutchens@gmail.com>
2025-01-31 11:51:44 -0800

    Fish prompt

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,6 +1,4 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-
     alias ls='eza --git --git-repos --classify --group-directories-first --smart-group --header'
     abbr --add ll 'ls -l'
     abbr --add la 'ls -a'
@@ -12,7 +10,40 @@ if status is-interactive
     abbr --add fdu 'cd $(ls_parents | fzf)'
     abbr --add fe 'hx $(bfs -type f | fzf -m)'
 
-    fish_config prompt choose astronaut
+    set fish_color_user magenta
+    set fish_color_cwd blue
+    
+    function fish_prompt
+        set -l last_status $status
+
+        set -l host (set_color $fish_color_host)"$hostname"(set_color normal)
+        set -l path  $(set_color $fish_color_cwd; short_path $PWD; set_color normal)
+        
+        set -l stat
+        if test $last_status -ne 0
+            set stat (set_color red)"[$last_status]"(set_color normal)
+        end
+
+        echo "$host $path$stat"
+        echo ">"
+    end
+
+    function postexec_test --on-event fish_postexec
+       echo
+    end
+end
+
+function short_path
+    set --local home_path (string escape --style=regex -- ~)
+
+    for path in $argv
+        set path (string replace -r '^'"$home_path"'($|/)' '~$1' $path)
+        set dirs $(string split '/' $path)
+        if test $(count $dirs) -gt 2
+            set dirs $dirs[1] "…" $dirs[-1]
+        end
+        echo $(string join "/" $dirs)
+    end
 end
 
 function ls_parents
