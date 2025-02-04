b514655fa3881300e84b68c9efc50db64642944c
Kyle Hutchens <k.d.hutchens@gmail.com>
2025-02-03 21:51:05 -0800

    Simplify setup

--- setup.zsh
+++ setup.zsh
@@ -1,14 +1,5 @@
 #! /usr/bin/env zsh
-
-dot_path=${0:a:h}/dotfiles
-
-declare -A dot_files
-dot_files=(
-    [$HOME/.config]=$dot_path/config
-    [$HOME/.zshrc]=$dot_path/zshrc
-    [$HOME/.gdbinit]=$dot_path/gdbinit
-    [$HOME/.tigrc]=$dot_path/tigrc
-)
+set -eu
 
 platform=$(uname)
 if [[ $platform = Darwin ]]; then
@@ -17,8 +8,17 @@ elif [[ $platform = Linux ]]; then
     ln_opts=-snv
 fi
 
-for link target in ${(kv)dot_files}; do
-    ln $ln_opts $target $link
-done
+function link {
+  target_path=$1
+  link_path=$HOME/.$target_path
 
-echo "\e[32mLinks created. Consider running additional setup if needed:\n\e[36m - homebrew_init.zsh\n - nvim_paq_init.zsh\e[0m"
+  mkdir -p $link_path:h
+  ln $ln_opts $target_path:a $link_path
+}
+
+cd $0:a:h/dotfiles
+
+link config/fish/config.fish
+link config/helix/config.toml
+link gdbinit
+link tigrc
