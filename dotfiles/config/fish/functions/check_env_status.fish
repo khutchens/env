8dd8c8154430ef58023fb3ec605dd24055522fc6
Kyle Hutchens <k.d.hutchens@gmail.com>
2025-02-06 09:49:05 -0800

    Fix env check

--- dotfiles/config/fish/functions/check_env_status.fish
+++ dotfiles/config/fish/functions/check_env_status.fish
@@ -1,5 +1,18 @@
 function check_env_status
-    echo Checking .env/ status...
-    git -C .env fetch
-    git -C .env status --untracked-files=no --short
+    echo "Checking .env/ status..."
+
+    set env_path ~/.env
+    git -C $env_path fetch
+
+    set branch_status $(git -C $env_path status --untracked-files=no | grep --only-matching "\(ahead\|behind\)[^,]*")
+    set local_status $(git -C $env_path status --untracked-files=no --short)
+
+    if test -n "$branch_status"
+        echo (set_color red)"Warning"(set_color normal)": Local .env is $branch_status"
+    end
+
+    if test -n "$local_status"
+        echo (set_color red)"Warning"(set_color normal)": Local .env is unclean:"
+        git -C $env_path status --untracked-files=no --short
+    end
 end
